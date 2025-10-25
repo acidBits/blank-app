fb82ac6d21c1c6f3c28f9f07f7e4b25cbe34b479
acidBits <43962792+acidBits@users.noreply.github.com>
2025-10-25 14:25:52 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ modelo = joblib.load("modelo.pkl")
 st.title("Conversor Temperatura ML")
 temp = st.text_input("Digite a temperatura em Celsius:")
 if st.button("Converter"):
-  saida = modelo.predict(temp)
+  saida = modelo.predict([temp])
   st.write(saida[0])
