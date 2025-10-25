7c2eb1b71bd12be20e6db31438024bd4a3edcd14
acidBits <43962792+acidBits@users.noreply.github.com>
2025-10-25 15:04:17 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,4 @@ temp = st.number_input("Digite a temperatura em Celsius:",value=0)
 
 if st.button("Converter"):
   saida = modelo.predict([[temp]])
-  st.write(f'{saida[0]:.2f} °F')
+  st.write(f'{saida[0]} °F')
