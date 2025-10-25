182b668485cf6bdfb2d07528400ac21386fbafac
acidBits <43962792+acidBits@users.noreply.github.com>
2025-10-25 14:01:04 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import joblib
 
-st.title("Teste Regressao Linear")
-st.write("ola")
+#carregando modelo
+url = "https://github.com/acidBits/blank-app/blob/main/modelo.pkl"
+r = requests.get(url)
+modelo = joblib.load("modelo.pkl")
+
+
+st.title("Conversor Temperatura ML")
+temp = st.text.input("Digite a temperatura em Celsius:")
+saida = modelo.predict([[temp]])
+st.write(saida[0])
