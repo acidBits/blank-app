3fc89aa72ed67ec48a6805a4075344f9bf8b8fca
acidBits <43962792+acidBits@users.noreply.github.com>
2025-10-25 14:18:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,7 @@ import joblib
 import requests
 
 #carregando modelo
-url = "https://github.com/acidBits/blank-app/blob/main/modelo.pkl"
-#r = requests.get(url)
-modelo = joblib.load(url)
-
+modelo = joblib.load("modelo.pkl")
 
 st.title("Conversor Temperatura ML")
 temp = st.text.input("Digite a temperatura em Celsius:")
